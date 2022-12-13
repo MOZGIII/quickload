3d69a729bb6a3846a66d6fff692dcc7bc13ba018
MOZGIII <mike-n@narod.ru>
2022-12-13 23:09:39 +0400

    More elegant declaration

--- src/disk_space_allocation.rs
+++ src/disk_space_allocation.rs
@@ -1,20 +1,17 @@
 use std::fs::File;
 
-#[cfg(all(unix, not(target_os = "macos")))]
-mod unix;
-#[cfg(windows)]
-mod windows;
-
-#[cfg(all(unix, target_os = "macos"))]
-mod fallback;
-
-#[cfg(all(unix, not(target_os = "macos")))]
-use self::unix as implementation;
-#[cfg(windows)]
-use self::windows as implementation;
-
-#[cfg(all(unix, target_os = "macos"))]
-use self::fallback as implementation;
+cfg_if::cfg_if! {
+    if #[cfg(all(unix, not(target_os = "macos")))] {
+        mod unix;
+        use self::unix as implementation;
+    } else if #[cfg(windows)] {
+        mod windows;
+        use self::windows as implementation;
+    } else {
+        mod fallback;
+        use self::fallback as implementation;
+    }
+}
 
 pub fn prepare_privileges() -> Result<(), anyhow::Error> {
     implementation::prepare_privileges()
