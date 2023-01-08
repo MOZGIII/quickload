77775927455dcfd1573782d420ec96d2ac2ee465
MOZGIII <mike-n@narod.ru>
2023-01-08 05:51:31 +0400

    Resolve clippy issues with linear-chunk-picker

--- crates/linear-chunk-picker/src/traits.rs
+++ crates/linear-chunk-picker/src/traits.rs
@@ -1,6 +1,8 @@
 //! Utility traits.
 
+/// Adds the ability to an increment that checks for overflow.
 pub trait CheckedIncrement: Sized + num_traits::One + num_traits::CheckedAdd {
+    /// Increment value and return it, or return `None` on overflow.
     fn checked_inc(&self) -> Option<Self>;
 }
 
